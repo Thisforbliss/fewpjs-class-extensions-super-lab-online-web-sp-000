1829fd9f15c662022869b599d09f8553957088f7
Malcolm Leake <echelon1089@gmail.com>
2020-04-14 22:32:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Tree {
     this.species = species
   }
   static definition() {
-    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Evergreens keep their leaves all year round."
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
   }
 }
 
